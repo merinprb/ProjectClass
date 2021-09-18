f3cd7fea0b1987c00c7f1b8406895ec3f0fe1b48
SheebaSelvaraj <sheebarajkumar92@gmail.com>
2021-09-18 15:14:34 +0530

    New Method Added in Class3

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,15 @@
 package com.git;
 
 public class Class3 {
+	private void coffee() {
+		System.out.println("coffee");
+}
 
+	private void tea() {
+		System.out.println("tea");
+}
+	private void milk() {
+		System.out.println("milk");
+
+	}
 }
