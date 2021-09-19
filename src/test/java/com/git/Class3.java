3cf90ed38c47b9a6822ec251f6f1f8599d22d8cb
Divikavi <diviskt6@gmail.com>
2021-09-19 17:04:45 +0530

    Divya updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,13 @@
 package com.git;
 
 public class Class3 {
-
+private void media1() {
+	System.out.println("1");
+}
+private void media2() {
+	System.out.println("2");
+}
+private void media3() {
+	System.out.println("3");
+}
 }
